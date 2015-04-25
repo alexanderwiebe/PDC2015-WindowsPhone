757d716da6b15e64a1bb9e2ef9ef5681ef92eb88
Alexander Wiebe <Alexander.Wiebe@solvera.ca>
2015-04-25 11:24:51 -0600

    deleted the chat tabs and stuff

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,17 +2,6 @@ angular.module('solfit.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
 .controller('LogCtrl', function($scope) {
     $scope.hello = 'sup';
 })
